241aae6f5c2312ee493933d87e1cc5dea6086504
Priyanshu Vishwakarma <144537220+Priyaanshhu@users.noreply.github.com>
2024-12-19 09:19:09 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,10 +1,17 @@
-def is_armstrong(n):
-num_str = str(n)
-power = len(num_str)
-sum_digits = sum(int(digit) ** power for digit in num_str)
-return sum_digits == n
-n = int(input("Enter a number: "))
-if is_armstrong(n):
-print(f"{n} is an Armstrong number.")
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num_str = str(num1) + str(num2) + str(num3)
+
+def armstrong(num1,num2,num3): 
+    value1 = num1*num1*num1
+    value2 = num2*num2*num2
+    value3 = num3*num3*num3
+    sum = value1+value2+value3
+    return sum
+  
+if int(num_str) == armstrong(num1,num2,num3):
+    print("This is armstrong number")
 else:
-print(f"{n} is not an Armstrong number.")
+    print("This is not armstrong number")
